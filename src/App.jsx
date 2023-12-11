485aa280f0083d3c8a1e9a58fc45d1180cb998b8
Thiem Huynh <thiem.huynh0@gmail.com>
2023-12-11 09:36:40 -0500

    basic functionality

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
 import { useState } from 'react'
 import './App.css'
 import Square from './components/Square'
+import Board from './components/Board'
 
 function App() {
   return (
     <>
-      <Square/>
+      <Board/>
     </>
   )
 }
